b5d8d293b423b393112aa57fb7e4480a87733654
a-poor <45295232+a-poor@users.noreply.github.com>
2022-01-04 10:02:18 -0800

    Updating shiny app

--- app.R
+++ app.R
@@ -8,45 +8,100 @@
 #
 
 library(tidyverse)
+library(readxl)
 library(shiny)
+
+data <- read_excel("data/SurveyData.xlsx") %>% 
+  mutate(
+    ResponseID = `Response ID`
+  ) %>% 
+  mutate(
+    
+  )
+meta <- read_excel("data/MBship22Jobs.xlsx")
+
+
+ind.var.set <- c(
+  "Gender" = "Gender",
+  "LGBTQ" = "LGBTQ"
+)
+dep.var.set <- c(
+  "Value" = "Value Received",
+  "Isolated" = "Felt Isolated",
+  "Belong" = "Feel Belong",
+  "DemeaningBehavior" = "Demeaning Behavior",
+  "Standoffish" = "Standoffish",
+  "ValueGenders" = "Values All Genders",
+  "Intimidating" = "Intimidating",
+  "Inclusive" = "Inclusive",
+  "OpValued" = "Opinion Valued",
+  "Recommend" = "Would Recommend",
+  "Renew" = "Likley to Renew",
+  "OverallSAT" = "Overall Satisfaction"
+)
 
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
+    titlePanel("Survey Data"),
+    
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+          selectInput(
+            "ind.var",
+            "Independant Variable",
+            ind.var.set,
+          ),
+          
+          selectInput(
+            "dep.var",
+            "Dependant Variable",
+            dep.var.set,
+          ),
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
            plotOutput("distPlot")
         )
-    )
+    ),
+    
+    dataTableOutput('table'),
+    
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-    
+    output$table <- renderDataTable(data, options = list(pageLength = 10))
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$plot <- renderPlot({
+      # generate bins based on input$bins from ui.R
+      df <- data %>% 
+        select(
+          input$ind.var,
+          input$dep.var
+        ) %>% drop_na()
+      
+      df %>% ggplot(aes_string(x = input$ind.var, y = input$dep.var)) + geom_bar()
+      
+      # x    <- data[, 2]
+      # bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      # 
+      # # draw the histogram with the specified number of bins
+      # hist(x, breaks = bins, col = 'darkgray', border = 'white')
     })
+    
+    # output$distPlot <- renderPlot({
+    #     # generate bins based on input$bins from ui.R
+    #     x    <- faithful[, 2]
+    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    # 
+    #     # draw the histogram with the specified number of bins
+    #     hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # })
 }
 
 # Run the application 
